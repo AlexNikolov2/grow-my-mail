0bbd53891aaf51dcf486260ca2af42f3523bc6a4
Aleks Nikolov <tcr.official.world@gmail.com>
2023-12-05 00:24:07 +0200

    left text for dropdowns

--- src/app/components/Dropdowns.tsx
+++ src/app/components/Dropdowns.tsx
@@ -1,4 +1,24 @@
 import Image from "next/image";
 
-export function DropdownsWhy() {}
-export function DropdownsHelp() {}
+export function DropdownsWhy() {
+  return (
+    <section className="dropdown-wrap">
+      <h2>Why us?</h2>
+      <section className="dropdown-items-wrap">
+        <section className="items-all">
+          <section className="item-dropdown">Predictability</section>
+          <section className="item-dropdown">Pay for results</section>
+          <section className="item-dropdown">Safety and security</section>
+          <section className="item-dropdown">Steady growth</section>
+          <section className="item-dropdown">Success, driven by math</section>
+          <section className="item-dropdown">Magic! (not quite)</section>
+          <section className="item-dropdown">Focused</section>
+        </section>
+        <section className="item-display"></section>
+      </section>
+    </section>
+  );
+}
+export function DropdownsHelp() {
+  return <></>;
+}
