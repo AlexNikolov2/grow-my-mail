9cbde223a4834d7f03f836df857f21f6a00df7cf
Aleks Nikolov <tcr.official.world@gmail.com>
2023-12-05 12:12:47 +0200

    working on dropdows

--- src/app/components/Dropdowns.tsx
+++ src/app/components/Dropdowns.tsx
@@ -14,7 +14,72 @@ export function DropdownsWhy() {
           <section className="item-dropdown">Magic! (not quite)</section>
           <section className="item-dropdown">Focused</section>
         </section>
-        <section className="item-display"></section>
+        <section className="item-display">
+          <section className="item-display-wrap">
+            <section className="item-display">
+              <p className="text-3xl">Predictability</p>
+              <p className="text-xl">
+                The average partner is proven to grow email sales by 106,4% in
+                the first 6 months of working with us.
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Pay for results</p>
+              <p className="text-xl">
+                Not for set-up.
+                <br /> Not for an office. <br /> Not for 50 employees.
+                <br /> Our philosophy: we make a lot of money when you make a
+                lot of money
+              </p>
+            </section>
+
+            <section className="item-display">
+              <p className="text-3xl">Safety and security</p>
+              <p className="text-xl">
+                Relax as we alleviate the stress of managing an in-house team
+                and the financial uncertainties of hiring and turnover.
+              </p>
+              <p className="text-xl">
+                We provide you with access to top-tier, world-class talent
+                that&apos;s second to none, removing the risk and ensuring your
+                peace of mind.
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Steady growth</p>
+              <p className="text-xl">
+                We’ll evolve at a pace you can keep up with; we won’t make a
+                revolution where we scale you up too quickly and you break the
+                logistic chain.
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Success, driven by math</p>
+              <p className="text-xl">
+                <p>We don’t rely on luck.</p>
+                <p>We engineer success based on data.</p>
+                <p>
+                  We analyze best-performing email campaigns and opportunities
+                  and capitalize on those.
+                </p>
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Magic! (not quite)</p>
+              <p className="text-xl">
+                The average partner is proven to grow email sales by 106,4% in
+                the first 6 months of working with us.
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Focused</p>
+              <p className="text-xl">
+                The average partner is proven to grow email sales by 106,4% in
+                the first 6 months of working with us.
+              </p>
+            </section>
+          </section>
+        </section>
       </section>
     </section>
   );
