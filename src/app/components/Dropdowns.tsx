2268a9a29dc1aaf8cfddfc2ae4036f0594b9de71
Aleks Nikolov <tcr.official.world@gmail.com>
2023-12-05 12:29:01 +0200

    setting up dropdowns help

--- src/app/components/Dropdowns.tsx
+++ src/app/components/Dropdowns.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export function DropdownsWhy() {
+  let [isOpened, setIsOpened] = useState();
+
   return (
     <section className="dropdown-wrap">
       <h2>Why us?</h2>
@@ -85,5 +88,86 @@ export function DropdownsWhy() {
   );
 }
 export function DropdownsHelp() {
-  return <></>;
+  return (
+    <section className="dropdown-wrap">
+      <h2>Why us?</h2>
+      <section className="dropdown-items-wrap">
+        <section className="items-all">
+          <section className="item-dropdown">Predictability</section>
+          <section className="item-dropdown">Pay for results</section>
+          <section className="item-dropdown">Safety and security</section>
+          <section className="item-dropdown">Steady growth</section>
+          <section className="item-dropdown">Success, driven by math</section>
+          <section className="item-dropdown">Magic! (not quite)</section>
+          <section className="item-dropdown">Focused</section>
+        </section>
+        <section className="item-display">
+          <section className="item-display-wrap">
+            <section className="item-display">
+              <p className="text-3xl">Predictability</p>
+              <p className="text-xl">
+                The average partner is proven to grow email sales by 106,4% in
+                the first 6 months of working with us.
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Pay for results</p>
+              <p className="text-xl">
+                Not for set-up.
+                <br /> Not for an office. <br /> Not for 50 employees.
+                <br /> Our philosophy: we make a lot of money when you make a
+                lot of money
+              </p>
+            </section>
+
+            <section className="item-display">
+              <p className="text-3xl">Safety and security</p>
+              <p className="text-xl">
+                Relax as we alleviate the stress of managing an in-house team
+                and the financial uncertainties of hiring and turnover.
+              </p>
+              <p className="text-xl">
+                We provide you with access to top-tier, world-class talent
+                that&apos;s second to none, removing the risk and ensuring your
+                peace of mind.
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Steady growth</p>
+              <p className="text-xl">
+                We’ll evolve at a pace you can keep up with; we won’t make a
+                revolution where we scale you up too quickly and you break the
+                logistic chain.
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Success, driven by math</p>
+              <p className="text-xl">
+                <p>We don’t rely on luck.</p>
+                <p>We engineer success based on data.</p>
+                <p>
+                  We analyze best-performing email campaigns and opportunities
+                  and capitalize on those.
+                </p>
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Magic! (not quite)</p>
+              <p className="text-xl">
+                The average partner is proven to grow email sales by 106,4% in
+                the first 6 months of working with us.
+              </p>
+            </section>
+            <section className="item-display">
+              <p className="text-3xl">Focused</p>
+              <p className="text-xl">
+                The average partner is proven to grow email sales by 106,4% in
+                the first 6 months of working with us.
+              </p>
+            </section>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
 }
