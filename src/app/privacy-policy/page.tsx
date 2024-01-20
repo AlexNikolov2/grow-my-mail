a142cb06f726fb3b84462e2db28cc57baf74a8ce
Aleks Nikolov <tcr.official.world@gmail.com>
2024-01-20 23:38:38 +0200

    adding privacy policy

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,69 @@
+export default function PrivacyPolicy() {
+  return (
+    <section className="privacy-policy">
+      <h1>PRIVACY POLICY</h1>
+      <section className="text">
+        <p>
+          ‍ At GrowMyMail, we are committed to protecting the privacy and
+          security of our users&apos; personal information. This Privacy Policy
+          outlines how we collect, use, disclose, and safeguard the personal
+          information provided to us through our website, services, and
+          communication channels. ‍
+        </p>
+        <p>
+          Information We Collect: We may collect various types of personal
+          information from you, including but not limited to your name, email
+          address, contact information, and any other information you provide to
+          us voluntarily. We may also collect non-personal information such as
+          browser type, IP address, and statistical data to enhance our website
+          and services. ‍
+        </p>
+        <p>
+          Use of Information: We use the information we collect to provide and
+          improve our services, personalize user experience, respond to
+          inquiries and requests, send promotional materials, and communicate
+          with you about our offerings. We may also use the information for
+          internal purposes such as data analysis, research, and improving our
+          marketing strategies.
+        </p>
+        ‍
+        <p>
+          Disclosure of Information: We do not sell, trade, or rent your
+          personal information to third parties without your consent. However,
+          we may share your information with trusted third-party service
+          providers who assist us in operating our business, conducting
+          campaigns, or servicing you. We may also disclose your information
+          when required by law or if we believe it is necessary to protect our
+          rights, property, or safety. ‍
+        </p>
+        <p>
+          Data Security: We implement reasonable security measures to safeguard
+          your personal information. However, no method of transmission or
+          electronic storage is completely secure, and we cannot guarantee
+          absolute security. You acknowledge and understand that we cannot
+          ensure the security of information transmitted to or stored on our
+          website or servers. ‍
+        </p>
+        <p>
+          External Links: Our website may contain links to third-party websites
+          or services. We are not responsible for the privacy practices or
+          content of such websites. We encourage you to review the privacy
+          policies of those third parties before providing any personal
+          information. ‍
+        </p>
+        <p>
+          Policy Updates: We reserve the right to update or modify this Privacy
+          Policy at any time without prior notice. Any changes will be effective
+          immediately upon posting on our website. We encourage you to review
+          this Policy periodically to stay informed about how we are protecting
+          your information. ‍
+        </p>
+        <p>
+          By using our website and services, you signify your consent to this
+          Privacy Policy. If you do not agree with this Policy, please refrain
+          from using our website or providing any personal information.
+        </p>
+      </section>
+    </section>
+  );
+}
